c7c23debe81a6954f4c3382996946f6a91092e94
Mitul Tandon <46290944+mitul01@users.noreply.github.com>
2024-02-07 00:35:26 +0530

    Add User CRUD operations (#6)

--- src/main/java/com/watchdog/watchdog/dto/UserInputDTO.java
+++ src/main/java/com/watchdog/watchdog/dto/UserInputDTO.java
@@ -4,10 +4,4 @@ import java.util.Objects;
 import java.util.UUID;
 
 public record UserInputDTO(String userName, String firstName, String lastName, UUID botId) {
-    public UserInputDTO {
-        Objects.requireNonNull(userName);
-        Objects.requireNonNull(firstName);
-        Objects.requireNonNull(lastName);
-        Objects.requireNonNull(botId);
-    }
 }
--- src/main/java/com/watchdog/watchdog/user/UseCases.java
+++ src/main/java/com/watchdog/watchdog/user/UseCases.java
@@ -3,8 +3,12 @@ package com.watchdog.watchdog.user;
 import com.watchdog.watchdog.dto.UserInputDTO;
 import com.watchdog.watchdog.model.User;
 
+import java.util.UUID;
+
 public interface UseCases {
      Iterable<User> users();
      User createUser(UserInputDTO userInput);
+     User updateUser(UUID userId,UserInputDTO userInput);
+     Boolean deleteUser(UUID userId);
 
 }
