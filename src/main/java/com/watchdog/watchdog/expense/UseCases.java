a159889b3ef139e090c0b44d1b67c34d820f6685
Mitul <mitultandon@gmail.com>
2024-02-08 23:28:38 +0530

    Add expense CRUD operations

---
+++ src/main/java/com/watchdog/watchdog/expense/UseCases.java
@@ -0,0 +1,13 @@
+package com.watchdog.watchdog.expense;
+
+import com.watchdog.watchdog.dto.ExpenseInputDTO;
+import com.watchdog.watchdog.model.Expense;
+
+import java.util.UUID;
+
+public interface UseCases {
+    Iterable<Expense> expenses();
+    Expense createExpense(ExpenseInputDTO expenseInput);
+    Expense updateExpense(UUID expenseId, ExpenseInputDTO expenseInput);
+    Boolean deleteExpense(UUID expenseId);
+}
